7842495bca738e15646953299fec3d4ccc1149d6
diggit <patrikbachan@gmail.com>
2012-12-06 20:53:17 +0100

    pressure read appended, val. differs from real, need fix

--- multi_sens/main.c
+++ multi_sens/main.c
@@ -29,18 +29,19 @@
 
 void log_str_ex(char *text,char NL);
 void log_redraw();
-void delay(unsigned int num) ;
+void delay(uint16_t num) ;
 void log_busyMSG(char text[]);
 void log_bar(short per);
-long calc_temp();
+int32_t calc_temp();
 void log_MSG(char status);
-void itos(unsigned int num, char *output);
-void log_num_ex(int number,char tmp);
+void itos(unsigned long num, char *output);
+void log_num_ex(long number,char tmp);
 int getW(char reg_addr);
 char getB(char reg_addr);
 void getCV();
+long calc_pressure();
 
-struct {
+struct { //downloaded calibration values
 	int16_t ac1;
 	int16_t ac2; 
 	int16_t ac3; 
@@ -54,12 +55,35 @@ struct {
 	int16_t md;
 }CV;
 
-
-
+struct{//calibration values calculated
+	unsigned long b7;
+	long b6;
+	long b5;
+	unsigned long b4;
+	long b3;
+} CVC;
 
 char DSP[4][24]={{""},{""},{""},{""}};
 uint8_t busyMSG=0,bar=0;
 
+const uint8_t resolution=2; //0 fastest - worst, 3 slowest - best
+
+
+void example_run()
+{
+	CVC.b5=2399;
+	CV.ac1=408;
+	CV.ac2=-72;
+	CV.ac3=-14383;
+	CV.ac4=32741;
+	CV.ac5=32757;
+	CV.ac6=23153;
+	CV.b1=6190;
+	CV.b2=4;
+	CV.mb=-32767;
+	CV.mc=-8711;
+	CV.md=2868;
+}
 
 int main (void)
 {
@@ -74,11 +98,20 @@ int main (void)
 	log_okMSG();
 	log_str("real temp");
 	//log_num(calc_temp());
+	
+	uint32_t tlak;
 	while(1)
 	{
-		//log_str("real");
-		log_numn(calc_temp());
+		//log_str("temperature:");
+		//log_num(calc_temp());
+		calc_temp();
+		delay(1000);
+		log_num(987654);
+		log_str("pressure:");
+		tlak=calc_pressure();
+		log_num(tlak);
 		delay(5000);
+		//CLEAN();
 	}	
 	
 	return 0;
@@ -122,42 +155,47 @@ int getW(char reg_addr)
 	return (raw_H<<8)|raw_L;
 }
 
-void log_num_ex(int number,char tmp)
+void log_num_ex(long number,char tmp)
 {
-	char buff[]="QQQQQ";
+	char buff[]="QQQQQQ";
 	itos(number,buff);
 	log_str_ex(buff,tmp);
 }
 
 
-void itos(unsigned int num, char *output)
+void itos(unsigned long num, char *output)
 {
 	//char output[]="12345";
 	
-	if (num>=10000)
-		output[0]='0'+num/10000;
+	if (num>=100000)
+		output[0]='0'+num/100000;
 	else
 		output[0]=' ';
-		
-	if (num>=1000)
-		output[1]='0'+num/1000%10;
+	
+	if (num>=10000)
+		output[1]='0'+num/10000%10;
 	else
 		output[1]=' ';
-	
-	if (num>=100)
-		output[2]='0'+num/100%10;
+		
+	if (num>=1000)
+		output[2]='0'+num/1000%10;
 	else
 		output[2]=' ';
 	
-	if (num>=10)
-		output[3]='0'+num/10%10;
+	if (num>=100)
+		output[3]='0'+num/100%10;
 	else
 		output[3]=' ';
 	
-	if (num>=1)
-		output[4]='0'+num%10;
+	if (num>=10)
+		output[4]='0'+num/10%10;
 	else
-		output[4]='X';
+		output[4]=' ';
+	
+	if (num>=1)
+		output[5]='0'+num%10;
+	else
+		output[5]='X';
 }
 
 
@@ -178,10 +216,10 @@ void log_MSG(char status)
 }
 
 
-long calc_temp()
+int32_t calc_temp()
 {
 	char err=0;//err test
-	long X1,X2;//buffers for translate vals into human readable
+	int32_t X1,X2;//buffers for translate vals into human readable
 	
 	//measure temp
 	//log_busyMSG("start measuring");
@@ -202,9 +240,74 @@ long calc_temp()
 	//log_str("raw");
 	//log_num(UT);
 	
-	X1=(((long)UT-(long)CV.ac6)*(long)CV.ac5)>>15;
-	X2=((long)CV.mc<<11)/(X1+(long)CV.md);
-	return((X1+X2+8)>>4);
+	X1=(((int32_t)UT-(int32_t)CV.ac6)*(int32_t)CV.ac5)>>15;
+	X2=((int32_t)CV.mc<<11)/(X1+(int32_t)CV.md);
+	CVC.b5=X1+X2;
+	return((CVC.b5+8)>>4);
+}
+
+long calc_pressure()
+{
+	char err=0;
+	long X1,X2,X3,p;
+	unsigned long raw_p;
+	
+	i2c_start(WA);
+	err+=i2c_write(0xF4);//we wanna write to control reg.
+	err+=i2c_write(0x34+(resolution<<6));//and measure pressure
+	i2c_stop();
+	
+	delay(1000);
+	
+	
+	//MUST BE MOVED AWAY into func.
+	
+	uint8_t raw_L=0,raw_H=0,raw_X=0;
+	
+	i2c_start(WA);//we wannay say what to read, that is writing... (OMG, silly mistake)
+	i2c_write(0xF6);//read from given addr
+	i2c_rep_start(RA);//restart i2c comm.
+	raw_H=i2c_read(1);//read byte and will be next
+	raw_L=i2c_read(1);//read byte and will be next
+	raw_X=i2c_read(0);//read byte and no next
+	i2c_stop();//stop
+	raw_p=( ((unsigned long) raw_H << 16) | ((unsigned long) raw_L << 8) | ((unsigned long) raw_X ) )>>(8-resolution); //merge all 3 bytes
+	
+
+	//example_run();
+	//raw_p=23843;
+	log_num(raw_p);
+	
+	delay(1000);
+	
+	//main calculation
+	CVC.b6=CVC.b5-4000;
+	X1=(CV.b2 * ((CVC.b6*CVC.b6)>>12))>>16;
+	X2=(CV.ac2 * CVC.b6)>>11;
+	X3=X1 + X2;
+	CVC.b3=(((((unsigned long) CV.ac1) * 4 +X3)<<resolution) + 2)>>2;
+	
+	X1=(CV.ac3 * CVC.b6)>>13;
+	X2=(CV.b1 * ((CVC.b6*CVC.b6)>>12))>>16;
+	X3=((X1 + X2) + 2)>>2;
+	CVC.b4=(CV.ac4 * (unsigned long)(X3 + 32768))>>15;
+	
+	CVC.b7=((unsigned long)(raw_p - CVC.b3) * (50000>>resolution));
+	if (CVC.b7 < 0x80000000)
+	{
+		p=(CVC.b7<<1) / CVC.b4;
+	}
+	else
+	{
+		p=(CVC.b7 / CVC.b4)<<1;
+	}
+	
+	X1=(p>>8)*(p>>8);
+	X1=(X1 * 3038)>>16;
+	X2=(-7357 * p)>>16;
+	p+=(X1 + X2 + 3791)>>4;
+
+	return p;
 }
 
 void log_bar(short per)
@@ -314,9 +417,9 @@ void log_redraw()
 			znak(DSP[j][i]);
 }
 
-void delay(unsigned int num) //simple delay loop
+void delay(uint16_t num) //simple delay loop
 {
-	unsigned int i,j;
+	uint16_t i,j;
 	for (i = 0; i < num; i++)
 		for (j=0;j<100;j++)
 		;
