2ef5493bd29da6d87be428649b2ab9169d2553e9
diggit <patrikbachan@gmail.com>
2012-12-08 23:34:12 +0100

    repo cleanup

--- multi_sens/dgtslib.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DGTSLIB_H_
-#define __DGTSLIB_H_
-
-char buffer[]="0123456789";//10 chars is enough, I hope...
-
-char *itos_NG(int32_t NUMBER, uint8_t min_chars, char *output);
-
-
-#endif
