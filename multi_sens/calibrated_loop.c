ba83353d87faeac1b3657d5413b25aa37c28dedf
diggit <patrikbachan@gmail.com>
2014-08-10 00:22:56 +0200

    timeout func added, macro cleanup

--- multi_sens/calibrated_loop.c
+++ multi_sens/calibrated_loop.c
@@ -3,11 +3,36 @@
 void delay_us(unsigned long delay)//takes 8 cycles, at 16MHz means half of micro second (freq*time)/repeated
 {
 	delay=delay<<1;//multiply by 2 to get 1uS delay
-	while(delay--) {_NOP;_NOP;}
+	while(delay--) {NOP;NOP;}
 };
 
 void delay_ms(uint16_t ms)
 {
 	for(;ms>0;ms--)
 		delay_us(1000);
+}
+
+uint8_t wait_timeout(volatile uint8_t *port, uint8_t mask, uint8_t timeout, uint8_t high)
+{
+	uint8_t counter;
+	for(counter=0;
+		((!(*port & mask) && high ) || //high level stop
+		((*port & mask) && !high )) //low level stop
+		&& counter!=timeout ;counter++)
+
+		NOP;
+	if(counter==timeout)
+	{
+		return 1;
+		#ifdef debug
+		uart_printl("STH timed out!");
+		#endif
+	}
+	else
+		return 0;
+}
+
+uint8_t wait_timeout_simple(volatile uint8_t *port, uint8_t mask)
+{
+	return wait_timeout(port,mask,255,1);
 }
--- multi_sens/calibrated_loop.h
+++ multi_sens/calibrated_loop.h
@@ -1,16 +1,15 @@
 #ifndef _LOOP_LIB
-        #define _LOOP_LIB
+	#define _LOOP_LIB
 
 
-        #include <inttypes.h>
+	#include <inttypes.h>
+	#include "misc.h"
 
-        #ifndef _NOP
-                #define _NOP asm volatile("NOP");
-        #endif
+	#define delay delay_ms
 
-        #define delay delay_ms
-        
-        void delay_ms(uint16_t ms);
-        void delay_us(unsigned long delay);//takes 8 cycles, at 16MHz means half of micro second (freq*time)/repeated
+	void delay_ms(uint16_t ms);
+	void delay_us(unsigned long delay);//takes 8 cycles, at 16MHz means half of micro second (freq*time)/repeated
+	uint8_t wait_timeout(volatile uint8_t *port, uint8_t mask, uint8_t timeout, uint8_t high);
+	uint8_t wait_timeout_simple(volatile uint8_t *port, uint8_t mask);
 
 #endif
