a34349206729402c353ed4bb39408bfdf9853a11
diggit <patrikbachan@gmail.com>
2014-08-10 00:27:05 +0200

    tested some new values

--- web_app/actual.js
+++ web_app/actual.js
@@ -1,5 +1,6 @@
 //dynamicaly generated, do not edit
-var last_temp= 18.40;
-var last_press= 1032;
-var last_humi= 46;
-var stamp="08.07. 2013 01:00";
+var last_temp= 29.80;
+var last_press= 1018;
+var last_humi= 33;
+var stamp="10.08. 2014 00:11";
+var status="OK :)";
