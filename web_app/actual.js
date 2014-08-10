d06cbd9533cc12758e9b6c195dfe4599374aa6c0
diggit <patrikbachan@gmail.com>
2014-08-10 23:37:31 +0200

    new file format testing

--- web_app/actual.js
+++ web_app/actual.js
@@ -1,6 +1,6 @@
 //dynamicaly generated, do not edit
-var last_temp= 29.80;
-var last_press= 1018;
-var last_humi= 33;
-var stamp="10.08. 2014 00:11";
-var status="OK :)";
+var last_temp= 30.70;
+var last_press= 1011;
+var last_humi= 35;
+var stamp="10.08. 2014 21:33";
+var status="ERR, contact admin!<span class="status_ok">i2c BUS: OK<\span><span class="status_ok">ADT module: OK<\span><span class="status_ok">ADT0 sensor: OK<\span><span class="status_ok">ADT1 sensor: OK<\span><span class="status_ok">ADT2 sensor: OK<\span><span class="status_ok">BMP sensor: OK<\span><span class="status_ok">DHT sensor: OK<\span><span class="status_ok">GENERAL status: OK<\span>";
