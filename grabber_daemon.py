428cef51cc30591baad52017878687b3a979d87b
diggit <patrikbachan@gmail.com>
2018-12-16 15:39:32 +0100

    more tweaks to plot size, add missing hr

--- grabber_daemon.py
+++ grabber_daemon.py
@@ -357,7 +357,7 @@ def gen_image(data,Xcol,Ycol,ImgName,LineColor, logY = False):
 	#plotting follows
 	if GENchart:
 		printDBG("IMG: generating plot")
-		pl.figure(figsize=(9,4), dpi=300)
+		pl.figure(figsize=(9,3.5), dpi=300)
 		printDBG("IMG: loading data")
 		x = pl.array(range(len(XlistVisible)))
 		y = pl.array(Ylist)
